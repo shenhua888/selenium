c8cfca5907717828f5070fe19a4e38d935faceeb
shenhua <shenhua266@sian.com>
2016-11-14 16:36:34 +0800

    org.jason类Ruby哈希的参数化

---
+++ src/test/java/com/test/first_maven/test/jackson/TestJason.java
@@ -0,0 +1,55 @@
+package com.test.first_maven.test.jackson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class TestJason {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestJason test = new TestJason();
+//		System.out.println(test.jsonTest2());
+		test.jsonTest();
+	}
+	public void jsonTest() throws JSONException{  
+	    String jsonString="{ \"name\":\"小民\","
+	    				  + "\"birthday\":\"1996年09月30日\","
+	    				  + "\"mail\":\"xiaomin@sina.com\","
+	    				  + "\"age\":28,"
+	    				  + "\"detail\":{\"name\":\"大大\",\"age\":88},"
+	    				  + "\"users\":[{\"loginname\":\"zhangfan\",\"password\":\"userpass\",\"email\":\"10371443@qq.com\"},{\"loginname\":\"zf\",\"password\":\"userpass\",\"email\":\"822393@qq.com\"}]}";  
+	    JSONObject json= new JSONObject(jsonString);
+	    System.out.println(json.get("name"));
+//	    System.out.println(json.get("birthday"));
+//	    System.out.println(json.get("mail"));
+//	    System.out.println(json.get("age"));	    
+	    System.out.println(((JSONObject) json.get("detail")).get("name"));
+	    JSONArray jsonArray=json.getJSONArray("users"); 
+	    for(int i=0;i<jsonArray.length();i++){  
+	        JSONObject user=(JSONObject) jsonArray.get(i);  
+	        System.out.println((String)user.get("loginname"));  
+	        
+	    }  
+	}  
+	
+	public String jsonTest2() throws JSONException{  
+	    String jsonString="{\"users\":[{\"loginname\":\"zhangfan\",\"password\":\"userpass\",\"email\":\"10371443@qq.com\"},{\"loginname\":\"zf\",\"password\":\"userpass\",\"email\":\"822393@qq.com\"}]}";  
+	    JSONObject json= new JSONObject(jsonString);  
+	    JSONArray jsonArray=json.getJSONArray("users");  
+	    String loginNames="loginname list:";  
+	    for(int i=0;i<jsonArray.length();i++){  
+	        JSONObject user=(JSONObject) jsonArray.get(i);  
+	        String userName=(String) user.get("loginname");  
+	        if(i==jsonArray.length()-1){  
+	            loginNames+=userName;  
+	        }else{  
+	            loginNames+=userName+",";  
+	        }  
+	    }  
+	    return loginNames;  
+	}  
+}
